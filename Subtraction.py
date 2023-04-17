c6d094cb1db613df47d4c6492094dd43d95c4ac4
Tanyyaaa24 <130543546+Tanyyaaa24@users.noreply.github.com>
2023-04-17 20:46:51 +0530

    Update Subtraction.py

--- Subtraction.py
+++ Subtraction.py
@@ -1,4 +1,5 @@
-# This function adds two numbers
-def subtract(x, y):
-    return x - y
+# This function adds two numbers.
+# n1 should be greater than n2.
+def subtract(n1, n2):
+    return n1 - n2
 
